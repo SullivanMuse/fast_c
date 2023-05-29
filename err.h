9f9d5a2ce9693aee0e9950692840a27add261fb2
Muse, Joshua S <sullivan.muse@gmail.com>
2023-05-29 16:21:33 -0500

    Add small error handling structure

---
+++ err.h
@@ -0,0 +1,32 @@
+#ifndef __ERR_H__
+#define __ERR_H__
+
+#include "box.h"
+#include "parsing.h"
+
+// Parse error node
+typedef struct
+{
+    Span span;
+    char *message;
+    ParseErrors *next;
+} ParseError;
+
+// Singly-linked list of parse errors
+// In order to use, simply declare
+//   ParseErrors errors;
+// Then pass this into whatever parser with &
+//   if (parse(s, &errors)) { ... }
+typedef ParseError *ParseErrors;
+
+// Prepend an error to the list
+void push_ParseError(Span span, char *message, ParseErrors *errors)
+{
+    ParseError out;
+    out.span = span;
+    out.message = message;
+    out.next = *errors;
+    *errors = (ParseErrors)BOX(out);
+}
+
+#endif
